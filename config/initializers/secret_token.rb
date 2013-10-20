20ab13b687a5b0930c12ea8845b560a9ee21b856
Thibaut Barrère <thibaut.barrere@gmail.com>
2013-10-20 09:54:08 +0200

    Add new Rails 4.0.0 app, as is

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsRecurlyJsSampleApplication::Application.config.secret_key_base = '1f5f3d530ba39b345da2fcc8bcf41a4f77935fc79b506b52b49d801b8fb24110ded7700940516151db0b31901dbb6dfdaee19bc74202db812f43ecd8eba21f51'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsRecurlyJsSampleApplication::Application.config.session_store :cookie_store, key: '_rails-recurly-js-sample-application_session'
