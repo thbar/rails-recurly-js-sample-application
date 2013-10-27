7a2d7a2ee74fa138e86450e152606aa386b5751b
Thibaut Barrère <thibaut.barrere@gmail.com>
2013-10-27 11:58:18 +0100

    Automatically generate recurly account code on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_validation :generate_recurly_account_code, on: :create
+  validates :recurly_account_code, uniqueness: true, presence: true
+
+  def generate_recurly_account_code
+    self.recurly_account_code ||= SecureRandom.uuid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'automatically generates a recurly_account_code on save' do
+    user = FactoryGirl.create(:user, :recurly_account_code => nil)
+    user.recurly_account_code.should_not be_blank
+  end
 end
