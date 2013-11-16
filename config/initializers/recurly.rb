b496e32425b52e8c90e0ca4fff14cddc9adc0e45
Thibaut Barrère <thibaut.barrere@gmail.com>
2013-11-16 23:24:02 +0100

    Make Travis happy

--- config/initializers/recurly.rb
+++ config/initializers/recurly.rb
@@ -1,5 +1,8 @@
-config = YAML.load(IO.read(Rails.root.join('config', 'settings.yml')))
+# For now, bypass during specs to keep Travis happy
+unless Rails.env.test?
+  config = YAML.load(IO.read(Rails.root.join('config', 'settings.yml')))
 
-Recurly.api_key          = config['recurly']['api_key']
-Recurly.js.private_key   = config['recurly']['js_private_key']
-Recurly.subdomain        = config['recurly']['subdomain']
+  Recurly.api_key          = config['recurly']['api_key']
+  Recurly.js.private_key   = config['recurly']['js_private_key']
+  Recurly.subdomain        = config['recurly']['subdomain']
+end
