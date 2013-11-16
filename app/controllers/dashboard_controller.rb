496fbaca23f2519a8558c5235ac4fd630f548935
Thibaut Barrère <thibaut.barrere@gmail.com>
2013-11-16 23:30:07 +0100

    Start using an html view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,4 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
   before_filter :request_subscription
-
-  def index
-    render text: 'You have access to all those paid features'
-  end
 end
