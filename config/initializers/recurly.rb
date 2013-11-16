232bf0e6403f24cfde835052e6c4f371545b306c
Thibaut Barrère <thibaut.barrere@gmail.com>
2013-11-16 22:10:29 +0100

    Configure the Recurly gem at initialization time

---
+++ config/initializers/recurly.rb
@@ -0,0 +1,5 @@
+config = YAML.load(IO.read(Rails.root.join('config', 'settings.yml')))
+
+Recurly.api_key          = config['recurly']['api_key']
+Recurly.js.private_key   = config['recurly']['js_private_key']
+Recurly.subdomain        = config['recurly']['subdomain']
